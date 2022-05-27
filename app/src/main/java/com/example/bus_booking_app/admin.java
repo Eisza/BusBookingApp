4c52fdfe3fb7f857a980102f6307c71ded4ae60d
eisa <g45@outlook.com>
2022-05-27 12:13:57 +0200

    Fixed signUp

--- app/src/main/java/com/example/bus_booking_app/admin.java
+++ app/src/main/java/com/example/bus_booking_app/admin.java
@@ -1,14 +1,31 @@
 package com.example.bus_booking_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.Activity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class admin extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/bus_booking_app/signup.java
+++ app/src/main/java/com/example/bus_booking_app/signup.java
@@ -1,14 +1,60 @@
 package com.example.bus_booking_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
+
+    EditText email ;
+    EditText pass;
+    Button signup;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        auth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.txtemail);
+        pass = findViewById(R.id.txtpass);
+        signup = findViewById(R.id.butsignup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_pass = pass.getText().toString();
+
+                registerUser(txt_email,txt_pass);
+            }
+        });
+    }
+    private void registerUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(signup.this ,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(signup.this, "Successful", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(signup.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
