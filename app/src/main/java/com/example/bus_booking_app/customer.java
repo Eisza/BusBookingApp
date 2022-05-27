8e5b9d86fcf30c5c88153ac3536d99542186fec5
eisa <g45@outlook.com>
2022-05-27 12:37:26 +0200

    LogIn (customer)

--- app/src/main/java/com/example/bus_booking_app/customer.java
+++ app/src/main/java/com/example/bus_booking_app/customer.java
@@ -1,14 +1,69 @@
 package com.example.bus_booking_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class customer extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button login;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser(email.getText().toString() , password.getText().toString());
+            }
+        });
+
     }
+
+    private void loginUser(String email , String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(customer.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(customer.this, "Authentication SUCCESS.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(customer.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
