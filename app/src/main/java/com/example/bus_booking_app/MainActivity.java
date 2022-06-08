44f9aa0d920a58ff2c9094b940a997783f6206de
eisa <g45@outlook.com>
2022-06-08 08:08:28 +0200

    admin update

--- app/src/main/java/com/example/bus_booking_app/MainActivity.java
+++ app/src/main/java/com/example/bus_booking_app/MainActivity.java
@@ -1,29 +1,70 @@
 package com.example.bus_booking_app;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     Button logout;
     Button admin;
+    ListView listview;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("Bus");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ////////////////////////////////LIST VIEW FOR BUSES////////////////////////
+        listview = findViewById(R.id.listview);
 
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.list_item,list);
+        listview.setAdapter(adapter);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                list.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+                
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+                //////////////////////////////////////////////
         admin = findViewById(R.id.adminsettings);
 
 
--- app/src/main/java/com/example/bus_booking_app/admin.java
+++ app/src/main/java/com/example/bus_booking_app/admin.java
@@ -1,51 +1,88 @@
 package com.example.bus_booking_app;
 
-import androidx.annotation.NonNull;
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
-import android.app.Activity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.text.NumberFormat;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class admin extends AppCompatActivity {
 
     Button add;
     Switch swt;
     EditText num;
+    EditText time;
+    RadioButton am;
+    RadioButton pm;
+    String bustime;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("Bus");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+
+        ///////////////retrieve data////////////////////
+
+
+
+
+        //////////////////////////////////
+
         num = findViewById(R.id.number);
-        add = findViewById(R.id.button);
-        swt = findViewById(R.id.switch1);
+        add = findViewById(R.id.add);
+        time = findViewById(R.id.time);
+        am = findViewById(R.id.AM);
+        pm = findViewById(R.id.PM);
+
+
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(am.isChecked()){
+                    bustime = time.getText().toString() + am.getText().toString();
+                }
+                else if(pm.isChecked()){
+                    bustime = time.getText().toString() + am.getText().toString();
+                }
+                else{
+                    Toast.makeText(admin.this, "please choose time", Toast.LENGTH_SHORT).show();
+                    bustime="";
+                }
 
-                for (int i =0; i < Integer.parseInt(num.getText().toString()); i++) {
-                    Toast.makeText(admin.this, "gg", Toast.LENGTH_SHORT).show();
+
+
+                if(Integer.parseInt(num.getText().toString()) > 0
+                        && Integer.parseInt(num.getText().toString()) < 10
+                        && bustime != ""
+                )
+
+                    for (int i =0; i < Integer.parseInt(num.getText().toString()); i++) {
 
                     FirebaseDatabase.getInstance().getReference()
-                            .child("Bus").child("1AM").child(String.valueOf(i+1))
-                            .setValue(swt.isChecked());
+                            .child("Bus").child(bustime).child(String.valueOf(i+1))
+                            .setValue(true);
                 }
             }
         });
